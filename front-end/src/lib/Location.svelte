14e694fdc82bba434459e74f46416158ce4cb958
tomasx420 <tomk0002@hz,nl>
2023-11-20 11:25:04 +0100

    get user location and make map component

---
+++ front-end/src/lib/Location.svelte
@@ -0,0 +1,17 @@
+<script>
+    let userLocation = null;
+  
+    async function getuserLocation() {
+      try {
+        const userLocation  = await new Promise((resolve, reject) => { 
+          navigator.geolocation.getCurrentPosition(resolve, reject);
+        });
+        return userLocation;
+      } 
+      catch (error) {
+        console.log(error);
+      }
+    }
+
+  </script>
+  
--- front-end/src/routes/+page.svelte
+++ front-end/src/routes/+page.svelte
@@ -1,29 +1,22 @@
 <script>
-    import { onMount } from 'svelte';
-  
-    let map;
-  
-    onMount(() => {
-      if (typeof window !== 'undefined') {
-        // Leaflet code that depends on the DOM
-        import('leaflet').then((L) => {
-          map = L.map('mapContainer').setView([51.505, -0.09], 13);
-          L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-          }).addTo(map);
-        });
-      }
-    });
-  </script>
-  
-  <svelte:head>
-    <link
-      rel="stylesheet"
-      href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
-      integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
-      crossorigin=""
-    />
-  </svelte:head>
-  
-  <div id="mapContainer" style="height: 400px; width: 50%" />
-  
+  import Map from "$lib/Map.svelte";
+  // import userLocation from "$lib/Location.svelte";
+
+  async function getuserLocation() {
+    try {
+      const position = await new Promise((resolve, reject) => { 
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+      });
+      return position;
+    } 
+    catch (error) {
+      console.log(error);
+      } 
+    }
+
+  console.log(getuserLocation());
+    
+    
+</script>
+
+<Map/>
