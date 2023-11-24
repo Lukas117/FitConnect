5a21662de703035dee9c5014d04feac8b3977781
João <nune0006@hz.nl>
2023-11-24 10:10:52 +0100

    pages login register sports and landing page

--- front-end/src/routes/+page.svelte
+++ front-end/src/routes/+page.svelte
@@ -1,4 +1,67 @@
-<h1>Welcome to FitConnect</h1>
-<p>
-	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<!-- src/routes/Landing.svelte -->
+
+<script>
+	// You can add any logic or data if needed
+  </script>
+  
+  <main>
+	<header>
+	  <h1>Welcome to FitConnection</h1>
+	</header>
+  
+	<section>
+	  <p>
+		Explore the amazing features of our platform. Here the more you interact the more fun u will have.
+	  </p>
+	</section>
+  
+	<section>
+	  <button>
+		Get Started
+	  </button>
+	</section>
+  </main>
+  
+  <style>
+	main {
+	  display: flex;
+	  flex-direction: column;
+	  align-items: center;
+	  justify-content: center;
+	  text-align: center;
+	  height: 100vh;
+	}
+  
+	header {
+	  margin-bottom: 24px;
+	}
+  
+	h1 {
+	  font-size: 2.5em;
+	  color: #333;
+	}
+  
+	p {
+	  font-size: 1.2em;
+	  color: #555;
+	  max-width: 600px;
+	  margin: 0 auto;
+	}
+  
+	button {
+	  background-color: #3498db;
+	  color: white;
+	  padding: 15px 30px;
+	  font-size: 1.2em;
+	  border: none;
+	  border-radius: 50px;
+	  cursor: pointer;
+	  transition-duration: 0.4s;
+	  padding-bottom: 10px;
+	}
+  
+	button:hover {
+	  background-color: #3498db;
+	}
+  </style>
+  
---
+++ front-end/src/routes/user/register/+page.svelte
@@ -0,0 +1,89 @@
+<!-- src/routes/Register.svelte -->
+
+<script>
+    let firstName = "";
+    let lastName = "";
+    let username = "";
+    let birthday = "";
+    let email = "";
+    let password = "";
+  
+    const handleRegister = () => {
+      // Implement your registration logic here
+      console.log("Registering with:", { firstName, lastName, username, birthday, email, password });
+  
+      // Add your registration logic here, e.g., send a request to your server
+      // and handle the registration response.
+    };
+  </script>
+  
+  <main>
+    <h1>Register</h1>
+  
+    <form on:submit|preventDefault={handleRegister}>
+      <label for="firstName">First Name:</label>
+      <input type="text" id="firstName" bind:value={firstName} required />
+  
+      <label for="lastName">Last Name:</label>
+      <input type="text" id="lastName" bind:value={lastName} required />
+  
+      <label for="username">Username:</label>
+      <input type="text" id="username" bind:value={username} required />
+  
+      <label for="birthday">Birthday:</label>
+      <input type="date" id="birthday" bind:value={birthday} required />
+  
+      <label for="email">Email:</label>
+      <input type="email" id="email" bind:value={email} required />
+  
+      <label for="password">Password:</label>
+      <input type="password" id="password" bind:value={password} required />
+  
+      <button type="submit">Register</button>
+    </form>
+  </main>
+  
+  <style>
+    main {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+    }
+  
+    h1 {
+      margin-bottom: 16px; /* Add some spacing below the heading */
+    }
+  
+    form {
+      max-width: 300px;
+      margin: auto;
+    }
+  
+    label {
+      display: block;
+      margin-bottom: 8px;
+    }
+  
+    input {
+      width: 100%;
+      padding: 8px;
+      margin-bottom: 16px;
+      box-sizing: border-box;
+    }
+  
+    button {
+      background-color: #3498db;
+      color: white;
+      padding: 10px 15px;
+      border: none;
+      border-radius: 5px;
+      cursor: pointer;
+    }
+  
+    button:hover {
+      background-color: #3498db;
+    }
+  </style>
+  
