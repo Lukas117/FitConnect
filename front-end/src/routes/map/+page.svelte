74cf6dfe093373357effc244393f44c209239976
João <nune0006@hz.nl>
2023-11-29 10:35:12 +0100

    tailwind instead of css

--- front-end/src/routes/map/+page.svelte
+++ front-end/src/routes/map/+page.svelte
@@ -1,9 +1,13 @@
 <script>
     import Map from "$lib/Map.svelte";
-    
     import Location from "$lib/Location.svelte";
-    
-    </script>
-    
-    <Map/>
-    <Location/>
+  </script>
+  
+  <main>
+    <div class="flex flex-col items-center bg-white p-8 rounded-lg shadow-lg">
+      <Map class="w-3/5 h-2/5 mb-4" />
+      <Location />
+    </div>
+  </main>
+  
+  
--- front-end/src/routes/user/sport/+page.svelte
+++ front-end/src/routes/user/sport/+page.svelte
@@ -1,42 +1,44 @@
 <script>
-    // Add any necessary logic or data here
-  </script>
-  
-  <style>
-    main {
-      display: flex;
-      flex-direction: column; /* Set flex-direction to column */
-      justify-content: center;
-      align-items: center;
-      height: 100vh;
-    }
-  
-    .sport-button {
-      background-color: #3498db;
-      color: #fff;
-      padding: 15px 20px;
-      margin: 10px;
-      border: none;
-      border-radius: 5px;
-      cursor: pointer;
-      font-size: 16px;
-      text-align: center;
-      text-decoration: none;
-      display: block; /* Set display to block */
-      transition-duration: 0.4s;
-    }
-  
-    .sport-button:hover {
-      background-color: #2980b9;
-    }
-  </style>
-  
-  <main>
+  let isSidebarOpen = false;
 
-    <h1>Choose a sport</h1>
-    <button class="sport-button" >Basketball</button>
-    <button class="sport-button" >Football</button>
-    <button class="sport-button" >Badminton</button>
-    <button class="sport-button" >Golf</button>
+  const toggleSidebar = () => {
+    isSidebarOpen = !isSidebarOpen;
+  };
+
+  const closeSidebar = () => {
+    isSidebarOpen = false;
+  };
+</script>
+
+<main class="flex flex-col justify-center items-center h-screen bg-background relative">
+  <div class="absolute top-0 right-0 m-4">
+    <!-- Hamburger Icon for Sidebar Toggle -->
+    <button 
+      class="text-3xl cursor-pointer"
+      on:click={toggleSidebar}
+    >
+      &#x2630;
+    </button>
+
+    <!-- Account Sidebar -->
+    {#if isSidebarOpen}
+      <div class="fixed top-0 right-0 h-screen w-1/3 bg-white shadow-md p-4 z-10">
+        <!-- Sidebar content goes here -->
+        <p class="block px-4 py-2 text-gray-800 hover:bg-gray-200" >Profile</p>
+        <p class="block px-4 py-2 text-gray-800 hover:bg-gray-200" on:click={closeSidebar}>Settings</p>
+        <p class="block px-4 py-2 text-gray-800 hover:bg-gray-200" on:click={closeSidebar}>Logout</p>
+      </div>
+    {/if}
+  </div>
+
+  <!-- Other content goes here -->
+  <main class="flex flex-col justify-center items-center h-screen bg-background">
+    <h1 class="text-2xl mb-4" style="primary;">Choose a sport</h1>
+    <a href="/map/">
+      <button class="sport-button bg-primary text-white w-24 h-10 rounded-md m-2 transition duration-400 ease-in-out hover:bg-accent">Basketball</button>
+    </a>
+    <button class="sport-button bg-primary text-white w-24 h-10 rounded-md m-2 transition duration-400 ease-in-out hover:bg-accent">Football</button>
+    <button class="sport-button bg-primary text-white w-24 h-10 rounded-md m-2 transition duration-400 ease-in-out hover:bg-accent">Badminton</button>
+    <button class="sport-button bg-primary text-white w-24 h-10 rounded-md m-2 transition duration-400 ease-in-out hover:bg-accent">Golf</button>
   </main>
-  
+</main>
