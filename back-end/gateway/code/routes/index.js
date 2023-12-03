ef4011fa03d017448a3aa7573accc100cc56e4e7
MelowRainn <ruij0036@hz.nl>
2023-12-03 20:52:12 +0100

    added dockerfiles

---
+++ back-end/gateway/code/routes/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+const router = express.Router();
+
+// create a proxy for each microservice
+const appointmentProxy = createProxyMiddleware({
+  target: 'http://localhost:3010',
+  changeOrigin: true,
+});
+
+
+router.use('/user', cors(), appointmentProxy);
+
+export default router;
---
+++ back-end/gateway/code/start.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+dotenv.config({ path: 'variables.env' });
+
+import indexRouter from './routes/index.js';
+import cors from 'cors';
+
+const app = express();
+
+// support json encoded and url-encoded bodies, mainly used for post and update
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', indexRouter);
+
+app.set('port', process.env.PORT || 3011);
+const server = app.listen(app.get('port'), () => {
+  console.log(` Express running → PORT ${server.address().port}`);
+});
