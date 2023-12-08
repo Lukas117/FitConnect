c0a53bb88902ba612a59402e978a29df447d5939
tomasx420 <tomk0002@hz,nl>
2023-12-08 15:48:50 +0100

    add comments

--- back-end/event/code/controllers/eventController.js
+++ back-end/event/code/controllers/eventController.js
@@ -2,18 +2,25 @@ import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
+
+// Get the directory name of the current module. This is necessary because __dirname is not available in ES modules.
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
+// Read the event.json file. fs.readFileSync is used to read the file synchronously.
 const rawData = fs.readFileSync(path.resolve(__dirname, './event.json'));
+// Parse the JSON data into a JavaScript object. JSON.parse is used to convert the JSON data into an object that can be used in the JavaScript code.
 const data = JSON.parse(rawData);
 
+// get all events (might not be working)
 export function getAllEventsHandler() {
   res.status(200).json(data.events);
 };
 
+// finds events by the id
 export const getEventById = (eventId) => {
   return data.events.find((e) => e.eventId === eventId);
 };
 
+// handles get events by id /api/events/:eventid
 export const getEventByIdHandler = (req, res) => {
   const eventId = req.params.eventId;
   const event = getEventById(eventId);
@@ -25,12 +32,14 @@ export const getEventByIdHandler = (req, res) => {
   }
 };
 
+// create event and write to event.json
 export const createEvent = (newEvent) => {
   data.events.push(newEvent);
   fs.writeFileSync(path.resolve(__dirname, './event.json'), JSON.stringify(data, null, 2), 'utf8');
   return newEvent
 };
 
+// hande the post request to create event
 export const createEventHandler = (req, res) => {
   const newEvent = req.body;
   const createdEvent = createEvent(newEvent);
--- back-end/event/code/start.js
+++ back-end/event/code/start.js
@@ -1,26 +1,37 @@
+// Import necessary modules
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import { router as eventRoutes } from './routes/eventRoutes.js';
 
+// Initialize express application
 const app = express();
+
+// Set the port for the server to listen on (the env might not work)
 const port = process.env.PORT || 3012;
 
+// enable CORS
 app.use(cors());
 
+// Use bodyParser middleware to parse JSON bodies
 app.use(bodyParser.json());
 
+// Use the eventRoutes for any requests to /api
 app.use('/api', eventRoutes);
 
+// handle 404 errors (might need to be in middleware)
 app.use((req, res, next) => {
-	try {
-		res.status(404).send('404 Not Found');
-	} catch (err) {
-		console.error('Error sending 404 response:', err);
-		next(err);
-	}
+    try {
+        // Send a 404 Not Found response
+        res.status(404).send('404 Not Found');
+    } catch (err) {
+        // Log any errors that occur when sending the response
+        console.error('Error sending 404 response:', err);
+        next(err);
+    }
 });
 
+// Start the server and listen on the specified port
 app.listen(port, () => {
-	console.log(`🏀   Service is running on port ${port}`);
+    console.log(`🏀   Service is running on port ${port}`);
 });
