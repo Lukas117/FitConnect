2f99688d19269d4679040284f7834b2aca9ff926
João <nune0006@hz.nl>
2023-12-08 16:08:04 +0100

    host pop up

--- front-end/src/lib/Map.svelte
+++ front-end/src/lib/Map.svelte
@@ -3,12 +3,40 @@
 	import { markerList, userLocation, size, icon } from '../store.js';
 	import { setIconOptions } from './iconUtility.js';
 	import Navigation from './Navigation.svelte';
+	import logo from '$lib/assets/logo.png';
 
 	let map;
 	let showError = false;
 	let showLoading = false;
 	let locationMarker;
 	let eventMarkersLayer;
+	let showModal = false;
+	let newEvent = {
+    lat: '',
+    lng: '',
+    title: '',
+    content: '',
+  	};
+
+	let showClockPicker = false;
+  let selectedTime = "00:00";
+
+  const hours = Array.from({ length: 24 }, (_, index) => index.toString().padStart(2, "0"));
+  const minutes = Array.from({ length: 60 }, (_, index) => index.toString().padStart(2, "0"));
+
+  function openClockPicker() {
+    showClockPicker = true;
+  }
+
+  function closeClockPicker() {
+    showClockPicker = false;
+  }
+
+  function saveTime() {
+    // Handle the selected time (selectedTime) logic here
+    closeClockPicker();
+  }
+
 
 	onMount(async () => {
 		// wait for the library to be imported
@@ -102,6 +130,11 @@
 		return L.marker(latlng, { icon: locationIcon });
 	}
 
+	//join event
+	function joinEvent(marker) {
+		
+	}
+
 	// create new event
 	function createEvent() {
 		const newMarker = {
@@ -114,7 +147,51 @@
 
 		// update the store by pushing the new marker
 		markerList.update((existingMarkers) => [...existingMarkers, newMarker]);
-	}
+
+		// Show the modal
+		showModal = true;
+  }
+
+ 		// Close the modal
+	function closeModal() {
+    // Close the modal and reset input values
+    showModal = false;
+    newEvent = {
+      lat: '',
+      lng: '',
+      title: '',
+      content: '',
+    };
+  	}
+
+	function saveEvent() {
+    const { lat, lng, title, content } = newEvent;
+
+    
+
+    const createdEvent = {
+      id: $markerList.length + 1,
+      lat: parseFloat(lat),
+      lng: parseFloat(lng),
+      title,
+      content,
+    };
+
+    // update the store by pushing the new marker
+    markerList.update((existingMarkers) => [...existingMarkers, createdEvent]);
+	console.log(createdEvent)
+
+	if (map) {
+    const marker = L.marker([createdEvent.lat, createdEvent.lng]).addTo(map);
+    marker.bindPopup(`<b>${createdEvent.title}</b><br>${createdEvent.content}`);
+    eventMarkersLayer.addLayer(marker);
+  }
+
+    // Close the modal
+    showModal = false;
+  }
+
+
 
 	// function to update markers on the map
 	function updateMarkers() {
@@ -138,6 +215,59 @@
 		}
 	});
 </script>
+<style>
+	/* Add styles for the overlay */
+	.overlay {
+	  position: fixed;
+	  top: 0;
+	  left: 0;
+	  width: 100%;
+	  height: 100%;
+	  background-color: rgba(0, 0, 0, 0.5);
+	  z-index: 1000;
+	  display: flex;
+	  align-items: center;
+	  justify-content: center;
+	}
+  
+	/* Add styles for the modal */
+	.modal {
+	  background-color: white;
+	  padding: 20px;
+	  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+	  border-radius: 10px; /* Adjust as needed */
+	}
+
+	.dropdown {
+    position: relative;
+    display: inline-block;
+  }
+
+  .dropdown-content {
+    display: none;
+    position: absolute;
+    max-height: 200px;
+    overflow-y: auto;
+    border: 1px solid #ccc;
+    background-color: #fff;
+    z-index: 1;
+  }
+
+  .dropdown-content a {
+    padding: 10px;
+    text-decoration: none;
+    display: block;
+    color: #333;
+  }
+
+  .dropdown-content a:hover {
+    background-color: #f1f1f1;
+  }
+
+  .dropdown:hover .dropdown-content {
+    display: block;
+  }
+  </style>
 
 <div class="relative bg-background" style="height: 600px; width: 100% z-0">
 	<div id="mapContainer" class="h-full w-full">
@@ -169,3 +299,130 @@
 		Create Event
 	</button>
 </section>
+
+{#if showModal}
+	
+  
+  <div class="fixed inset-0 z-50 flex items-center justify-center" style="z-index: 1000">
+  
+
+  <!-- Increase the size of the modal container -->
+  <div class="z-40 relative p-8 bg-white rounded-md shadow-md">
+    <img alt="The project logo" src={logo} class="w-16 h-16 mb-4" />
+    <h2 class="text-2xl font-bold mb-4">Name of Event</h2>
+      <form on:submit|preventDefault={saveEvent} class="space-y-4">
+		
+        <h1 class="text-orange-700">Invite our friends:</h1>
+		<div class="bg-orange-200">
+			<div class="flex flex-col mb-4">
+          
+				<select id="friend" name="friend" class="p-2 border bg-orange-400 rounded">
+				  <option value="friend1">Friend 1</option>
+				  <option value="friend2">Friend 2</option>
+				  <option value="friend3">Friend 3</option>
+				  <option value="friend4">Friend 4</option>
+				  
+				</select>
+			  </div>
+			  <div class="flex flex-col mb-4">
+				  <select id="friend" name="friend" class="p-2 border bg-orange-400 rounded">
+					<option value="friend1">Friend 1</option>
+					<option value="friend2">Friend 2</option>
+					<option value="friend3">Friend 3</option>
+					<option value="friend4">Friend 4</option>
+					
+				  </select>
+			  </div>
+			  <div class="flex flex-col mb-4">
+				  
+				  <select id="friend" name="friend" class="p-2 border bg-orange-400 rounded">
+					<option value="friend1">Friend 1</option>
+					<option value="friend2">Friend 2</option>
+					<option value="friend3">Friend 3</option>
+					<option value="friend4">Friend 4</option>
+					
+				  </select>
+			  </div>
+		  
+			  <div class="flex flex-col mb-4">
+				  <select id="friend" name="friend" class="p-2 border bg-orange-400 rounded">
+						<option value="friend1">Friend 1</option>
+						<option value="friend2">Friend 2</option>
+						<option value="friend3">Friend 3</option>
+						<option value="friend4">Friend 4</option>
+				  </select>
+			  </div>
+			  <div class="flex flex-col mb-4">
+				  <select id="friend" name="friend" class="p-2 border bg-orange-400 rounded">
+						<option value="friend1">Friend 1</option>
+						<option value="friend2">Friend 2</option>
+						<option value="friend3">Friend 3</option>
+						<option value="friend4">Friend 4</option>
+				  </select>
+			  </div>
+		</div>
+		<div class="flex justify-between">
+			<div class="flex flex-col mb-4" style="width: 200px;"> <!-- Adjust width as needed -->
+				<div class="relative">
+					
+					<select id="friend" name="PICK THE PLACE" class="p-2 border bg-white-400 rounded">
+						<option value="FACILITY1">FACILITY1</option>
+						<option value="FACILITY2">FACILITY2</option>
+						<option value="FACILITY3">FACILITY3</option>
+						<option value="FACILITY4">FACILITY4</option>
+					</select>
+				</div>
+			</div>
+		
+			<div class="flex flex-col mb-4" style="width: 200px;"> <!-- Adjust width as needed -->
+				<div class="relative">
+					
+					<input type="date" id="birthday" required class="w-full px-3 py-2 mb-4 border border-gray-300 rounded-md" />
+				</div>
+			</div>
+		
+			<div class="dropdown pl-10" style="width: 200px;"> <!-- Adjust width as needed -->
+				<button on:click={() => openClockPicker()} class="bg-orange-400 text-white px-10 py-2 rounded hover:bg-black-600 pl-10 pr-10">
+					{selectedTime}
+				</button>
+				{#if showClockPicker}
+					<div class="dropdown-content">
+						<div class="flex justify-between">
+							<!-- Hours dropdown -->
+							<div class="flex flex-col">
+								{#each hours as hour (hour)}
+									<a on:click={() => selectedTime = `${hour}:${selectedTime.split(":")[1]}`}>{hour}</a>
+								{/each}
+							</div>
+							<!-- Minutes dropdown -->
+							<div class="flex flex-col">
+								{#each minutes as minute (minute)}
+									<a on:click={() => selectedTime = `${selectedTime.split(":")[0]}:${minute}`}>{minute}</a>
+								{/each}
+							</div>
+						</div>
+					</div>
+				{/if}
+			</div>
+		</div>
+		
+		<div class="flex justify-between items-center mb-4">
+			<!-- Cancel button on the left -->
+			<button type="button" on:click={closeModal} class="text-orange-500 hover:text-gray-700 px-4 py-2 rounded">Cancel</button>
+		
+			<!-- Save button in the middle -->
+			<button type="submit" class="bg-orange-700 text-white px-4 py-2 rounded hover:bg-blue-600">Save</button>
+		
+			<!-- Icon for definitions on the right -->
+			<div class="flex items-center">
+				<!-- Replace "YourIconComponent" with your actual icon component or SVG -->
+				<button type="button" on:click={closeModal} class="text-gray-500 hover:text-gray-700 px-4 py-2 rounded">Definitions</button>
+			</div>
+		</div>
+
+      </form>
+    </div>
+  </div>
+{/if}
+
+
--- front-end/src/routes/map/+page.svelte
+++ front-end/src/routes/map/+page.svelte
@@ -1,10 +1,15 @@
 <script>
-  
 	import Map from '$lib/Map.svelte';
 	import Location from '$lib/Location.svelte';
+  </script>
   
-</script>
-<div class="w-full h-full bg-background">
-	<Map />
-	<Location />
-</div>
+  <div class="relative z-0">
+	<div class="w-full h-full bg-background z-10 relative">
+	  <Map />
+	  <Location />
+	</div>
+  </div>
+  
+	
+  
+  
