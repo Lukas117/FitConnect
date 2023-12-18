4cae1b4d1b8aceb71e9705c5d917c39caaaaaa1d
Clint <esch0007@hz.nl>
2023-12-18 14:10:16 +0100

    Refactor methods to be more isolated

--- back-end/user/code/controllers/userController.js
+++ back-end/user/code/controllers/userController.js
@@ -6,23 +6,17 @@ const supabaseUrl = "https://nhrrgnjkvzxceshaiwih.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ocnJnbmprdnp4Y2VzaGFpd2loIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwMDc0MjIxMSwiZXhwIjoyMDE2MzE4MjExfQ.iKmh3-Zlka-WfYDrG0-DcS_WCW6gIEqz5XPIbw4rF9o"
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-export async function register(req, res) {
+const errorStrings = {
+  500: 'Internal Server Error'
+}
 
-  const { name, user_name, birth_date, email, password_hash } = req.body;
+function errorResponse(errorCode, errorTitle, errorMessage) {
+  console.error('Error ', errorTitle, errorMessage);
+  return res.status(errorCode).json({ error: errorStrings[errorCode] });
+}
 
-  const { data: existingUsers, error: existingUsersError } = await supabase
-    .from('user')
-    .select()
-    .or('email.eq.' + email, 'user_name.eq.' + user_name);
-
-  if (existingUsersError) {
-    console.error('Error checking existing users:', existingUsersError.message);
-    return res.status(500).json({ error: 'Internal Server Error' });
-  }
-
-  if (existingUsers.length > 0) {
-    return res.status(400).json({ error: 'Email or user_name already exists' });
-  }
+async function userCreateAsync(userData) {
+  const { name, user_name, birth_date, email, password_hash } = userData;
 
   const newUser = {
     name,
@@ -34,146 +28,142 @@ export async function register(req, res) {
   };
 
   try {
-    const password_salt = crypto.randomBytes(16).toString('hex');
-    const passwordBuffer = Buffer.from(password_hash, 'utf8');
-    const saltBuffer = Buffer.from(password_salt, 'hex');
-    const N = 16384; // CPU/memory cost parameter
-    const r = 8; // block size parameter
-    const p = 1; // parallelization parameter
-    const dkLen = 64; // derived key length
-
-    // Use the scrypt implementation from the crypto module
-    const derivedKey = await new Promise((resolve, reject) => {
-      crypto.scrypt(passwordBuffer, saltBuffer, dkLen, (err, derivedKey) => {
-        if (err) reject(err);
-        else resolve(derivedKey);
-      });
-    });
-
-    newUser.password_hash = derivedKey.toString('hex');
-    newUser.password_salt = password_salt;
-
-    // Save the new user to the 'users' table in Supabase
-    const { data, error } = await supabase.from('user').upsert([newUser]);
-
-    if (error) {
-      console.error('Error creating user:', error.message);
-      return res.status(500).json({ error: 'Internal Server Error' });
-    }
-
-    const { data: user, error: userError } = await supabase
-      .from('user')
-      .select('*')
-      .eq('user_name', newUser.user_name)
-
-    if (userError) {
-      console.error('Error fetching user:', userError.message);
-      return res.status(500).json({ error: 'Internal Server Error' });
-    }
-
-    // Generate JWT token
-    const token = jwt.sign({ userId: user.id }, 'wompwomp', { expiresIn: '24h' });
-
-    res.status(200).json({ user, token });
+    newUser.password_hash, newUser.password_salt = await userPasswordGenerateAsync(password_hash);
   } catch (error) {
-    console.error('Error creating user:', error.message);
-    res.status(500).json({ error: 'Internal Server Error' });
+    return null, errorResponse(400, 'Password generation: ', error.message);
+  }
+  
+  try {
+    const { data, error } = await supabase
+      .from('user')
+      .upsert([newUser]);
+
+    if (error) throw error
+    else return data, null;
+  } catch (error) {
+    return null, errorResponse(500, 'Error creating user:', error.message);
   }
 }
 
-export async function getUserById(req, res) {
+async function userFindEmailUsernameAsync(userData) {
+  const { email, user_name } = userData;
+  return await supabase
+    .from('user')
+    .select('*')
+    .or('email.eq.' + email, 'user_name.eq.' + user_name);
+}
+
+async function userPasswordGenerateAsync(hash, salt) {
+  const password_salt = salt ? salt : crypto.randomBytes(16).toString('hex');
+  const passwordBuffer = Buffer.from(hash, 'utf8');
+  const saltBuffer = Buffer.from(password_salt, 'hex');
+  const N = 16384; // CPU/memory cost parameter
+  const r = 8; // block size parameter
+  const p = 1; // parallelization parameter
+  const dkLen = 64; // derived key length
+
+  // Use the scrypt implementation from the crypto module
+  const derivedKey = await new Promise((resolve, reject) => {
+    crypto.scrypt(passwordBuffer, saltBuffer, dkLen, (err, derivedKey) => {
+      if (err) reject(err);
+      else resolve(derivedKey);
+    });
+  });
+
+  return derivedKey.toString('hex'), password_salt;
+}
+
+async function getUserFromFieldAsync(name, valueMatch) {
+  try {
+    const { data, error } = await supabase.from('user').select('*').eq(name, valueMatch);
+    if (error) throw error;
+    else return data, null
+  } catch (error) {
+    return null, errorResponse(500, 'Error fetching user:', error.message);
+  }
+}
+
+async function setUserFieldFromFieldAsync(fieldlist, fieldName, valueMatch) {
+  try {
+    const { user, error } = await supabase.from('user').select(fieldlist).eq(fieldName, valueMatch);
+    if (error) throw error;
+    else return user, null
+  } catch (error) {
+    return null, errorResponse(500, 'Error fetching user:', error.message);
+  }
+}
+
+export async function userRegister(req, res) {
+  let existingUser, existingUserError = await userFindEmailUsernameAsync(req.body);
+
+  if (existingUserError) {
+    return errorResponse(500, 'Error checking existing users:', existingUserError.message);
+  }
+
+  if (existingUser.length > 0) {
+    return errorResponse(400, 'Error checking existing users:', 'Email or user_name has already been taken.');
+  }
+
+  let user, userCreationError = await userCreateAsync(req.body);
+  if (userCreationError) return userCreationError;
+
+  const token = jwt.sign({ userId: user.id }, 'wompwomp', { expiresIn: '24h' });
+
+  return res.status(200).json({ user, token });
+}
+
+export async function getUserFromId(req, res) {
   // Logic for fetching a user by userId
   const userId = req.params.id;
 
   try {
-    // Fetch the user with the matching userId from the 'users' table
-    const { data, error } = await supabase.from('user').select('*').eq('userId', userId);
+    let user, userError = await getUserFromFieldAsync('id', userId);
+    if (userError) return userError;
 
-    if (error) {
-      console.error('Error fetching user:', error.message);
-      return res.status(500).json({ error: 'Internal Server Error' });
-    }
-
-    const user = data[0];
+    console.log(user[0])
 
     if (!user) {
-      return res.status(404).json({ error: 'User not found' });
+      throw {message: 'Could not locate the user based on the id: ' + toString(userId)}
     }
 
-    res.status(200).json(user);
+    return res.status(200).json(user);
   } catch (error) {
-    console.error('Error fetching user:', error.message);
-    res.status(500).json({ error: 'Internal Server Error' });
+    return errorResponse(500, 'Error fetching user:', error.message);
   }
 }
 export async function login(req, res) {
-  // Logic for user login
   const { email, password } = req.body;
 
-  if (!email || !password) {
-    return res.status(400).json({ error: 'Email and password are required' });
-  }
+  if (email && password) {
+    const { user, error } = await getUserFromFieldAsync('email', email);
+    if (error) return errorResponse(500, 'Error fetching user:', error.message);
 
-  try {
-    // Fetch the user with the matching email from the 'users' table
-    const { data, error } = await supabase.from('user').select('*').eq('email', email);
-
-    if (error) {
-      console.error('Error fetching user:', error.message);
-      return res.status(500).json({ error: 'Internal Server Error' });
-    }
-
-    const user = data[0];
+    console.log(data[0]);
 
     if (!user) {
-      return res.status(401).json({ error: 'Invalid credentials' });
+      throw errorResponse(500, 'Error fetching user:', 'Invalid credentials');
     }
 
-    // Verify the password
-    const passwordBuffer = Buffer.from(password, 'utf8');
-    const saltBuffer = Buffer.from(user.password_salt, 'hex');
-    const dkLen = 64; // derived key length
+    const { password_hash } = await userPasswordGenerateAsync(password, user.password_salt)
+    if (password_hash) {
+      const token = jwt.sign({ userId: user.id }, 'wompwomp', { expiresIn: '24h' });
 
-    const derivedKey = await new Promise((resolve, reject) => {
-      crypto.scrypt(passwordBuffer, saltBuffer, dkLen, (err, key) => {
-        if (err) reject(err);
-        else resolve(key);
-      });
-    });
-
-    const hashedPassword = derivedKey.toString('hex');
-
-    if (hashedPassword !== user.password_hash) {
-      return res.status(401).json({ error: 'Invalid credentials' });
+      return res.status(200).json({ user, token });
+    } else {
+      throw errorResponse(500, 'Error fetching user:', 'Invalid credentials');
     }
-
-    // Password is valid, generate JWT token
-    const token = jwt.sign({ userId: user.id }, 'wompwomp', { expiresIn: '24h' });
-
-    res.status(200).json({ user, token });
-  } catch (error) {
-    console.error('Error fetching user:', error.message);
-    res.status(500).json({ error: 'Internal Server Error' });
+  } else {
+    return errorResponse(400, 'Email and password are required.');
   }
-
 }
+
+
 export async function addSport(req, res) {
   const { sport_list, email } = req.body;
   console.log(sport_list);
   console.log(email);
-  try {
-    const { data, error } = await supabase
-      .from('user')
-      .update({ sport_list: sport_list })
-      .eq('email', email);
 
-    if (error) {
-      console.error('Error adding sport:', error.message);
-      return res.status(500).json({ error: 'Internal Server Error' });
-    }
-    res.status(201).json({ sport: sport_list });
-  } catch (error) {
-    console.error('Error adding sport:', error.message);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
+  const { user, error } = await setUserFieldFromFieldAsync({ sport_list: sport_list }, 'email', email)
+  if (error) return errorResponse(500, 'Error adding sport:', error.message); 
+  else return res.status(201).json({ sport: sport_list });
 };
