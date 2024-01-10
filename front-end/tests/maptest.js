31457ea7c71bb29c4bf6971d7d3e07e229f6e634
tomasx420 <tomk0002@hz,nl>
2024-01-10 15:41:57 +0100

    fixing map ui test

--- front-end/tests/maptest.js
+++ front-end/tests/maptest.js
@@ -1,20 +1,27 @@
 import { expect, test } from '@playwright/test';
 
 test('map and UI renders without errors', async ({ page }) => {
+	// Wait for the page to load
 	await page.goto('/map');
-
-	const mapContainer = await page.locator('#mapContainer');
+	await page.waitForLoadState('load');
 
 	// Check if the map container is visible
+	const mapContainer = await page.locator('#mapContainer');
 	await expect(mapContainer).toBeVisible();
 
 	// Check if the navigation button is visible
-	await page.waitForSelector('#navigationButton');
+	await page.waitForSelector('#navigationButton', {
+		state: 'visible',
+		timeout: 5000
+	});
 	const navigationButton = await page.locator('#navigationButton');
 	await expect(navigationButton).toBeVisible();
 
 	// Check if the host button is visible
-	await page.waitForSelector('#hostButton');
+	await page.waitForSelector('#hostButton', { 
+		state: 'visible', 
+		timeout: 5000 
+	});
 	const hostButton = await page.locator('#hostButton');
 	await expect(hostButton).toBeVisible();
 });
