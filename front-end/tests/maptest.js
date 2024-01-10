63f8d396046f776daf341600c0d94721c44a2016
tomasx420 <tomk0002@hz,nl>
2024-01-10 15:30:03 +0100

    update map ui test

--- front-end/tests/maptest.js
+++ front-end/tests/maptest.js
@@ -2,17 +2,19 @@ import { expect, test } from '@playwright/test';
 
 test('map and UI renders without errors', async ({ page }) => {
 	await page.goto('/map');
-  
+
 	const mapContainer = await page.locator('#mapContainer');
-  
+
 	// Check if the map container is visible
 	await expect(mapContainer).toBeVisible();
-  
+
 	// Check if the navigation button is visible
+	await page.waitForSelector('#navigationButton');
 	const navigationButton = await page.locator('#navigationButton');
 	await expect(navigationButton).toBeVisible();
-  
+
 	// Check if the host button is visible
+	await page.waitForSelector('#hostButton');
 	const hostButton = await page.locator('#hostButton');
 	await expect(hostButton).toBeVisible();
 });
